676885989dea4d6077551b0480162d76be0efc58
Gaurav Gupta <gouravgupta840@gmail.com>
2018-08-20 12:39:01 +0530

    Finish app

--- 05 Diving Deeper into Components & React Internals/src/components/Persons/Person/Person.js
+++ 05 Diving Deeper into Components & React Internals/src/components/Persons/Person/Person.js
@@ -17,6 +17,9 @@ class Person extends Component {
 
     componentDidMount () {
         console.log( '[Person.js] Inside componentDidMount()' );
+        if ( this.props.position === 0 ) {
+            this.inputElement.focus();
+        }
     }
 
     render () {
@@ -25,7 +28,11 @@ class Person extends Component {
             <Aux>
                 <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.changed} value={this.props.name} />
+                <input
+                    ref={( inp ) => { this.inputElement = inp }}
+                    type="text"
+                    onChange={this.props.changed}
+                    value={this.props.name} />
             </Aux>
         )
         // return [
@@ -40,7 +47,7 @@ Person.propTypes = {
     click: PropTypes.func,
     name: PropTypes.string,
     age: PropTypes.number,
-    changed: PropTypes.func 
+    changed: PropTypes.func
 };
 
-export default withClass(Person, classes.Person);
+export default withClass( Person, classes.Person );
